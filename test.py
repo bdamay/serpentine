4f65478fbbf5b605f0e607c73533043396b58128
bdamay <bdamay@54f7f2d0-5556-0156-5b9a-215845ccd781>
2014-04-25 18:38:21 +0000

    tests on matching segments

--- test.py
+++ test.py
@@ -8,18 +8,31 @@ def run_test():
     get_matching_segments()
 
 
+
 def get_matching_segments():
     from gps.models import Trace, Trace_point
-    from gps import lib
+
+    def get_matching_point(t1):
+        matches = {}
+        tps = Trace_point.objects.exclude(trace=2)
+        tps= tps.extra(where=['20000*(abs('+str(t1.latitude)+'-latitude)+abs('+str(t1.longitude)+'-longitude)) < 1'])
+        tps = tps.order_by('order_num')
+        if tps.count()>0:
+            matches[t1] = tps[0]
+        return matches
 
     alltps = Trace_point.objects.all()
-    t1 = alltps.filter(trace=2)[0]
-    print t1
-    tps = Trace_point.objects.exclude(trace=2)
-    tps= tps.extra(where=['20000*(abs('+str(t1.latitude)+'-latitude)+abs('+str(t1.longitude)+'-longitude)) < 1'])
-    tps = tps.order_by('order_num')
-    # if tps.count()>0:
-    print tps[0]
+    t1 = alltps.filter(trace=2)[1]
+    matches = get_matching_point(t1)
+    print matches
+
+    t1 = alltps.filter(trace=2)[10]
+    matches = get_matching_point(t1)
+    print matches
+
+    t1 = alltps.filter(trace=2)[30]
+    matches = get_matching_point(t1)
+    print matches
 
 start = datetime.now()
 print start
