7e1904cc043f949a9039246d27fdabf84ffcf6c4
bdamay <bdamay@54f7f2d0-5556-0156-5b9a-215845ccd781>
2015-01-29 14:28:31 +0000

    Refonte js

--- gps/static/js/spt/serpentine.js
+++ gps/static/js/spt/serpentine.js
@@ -1,10 +1,10 @@
 /* main js for serpentine */
 $(document).ready(function() {
-    //un track est défini
-    SPT = Ui({
-        mainmap_div: 'map_canvas',
-        maintrack: ((typeof track_id !== 'undefined')? track_id : null)
-    });
+    //check du page_ui passé par la page
+    if (typeof sptui !== 'object') {
+        sptui = {type: 'base'}
+    }
+    SPT = Ui(sptui);
     SPT.initialise();
     // alert(SPT);
 });
@@ -13,19 +13,20 @@ $(document).ready(function() {
  *  TRACK UI object
  *  */
 var Ui = function(spec,my) {
+    /** attributs **/
     var that = {};
+    that.type = (spec.hasOwnProperty('type') ? spec.type : 'base'); // l'objet instancié contient de base les propriétés passées
+    that.track_id = (spec.hasOwnProperty('track_id') ? spec.track_id : -1); // l'objet instancié contient de base les propriétés passées
     that.width = $(document).width();
     that.height = $(document).height();
-    if (spec.maintrack !== null){
-        that.maintrack = Track({id:spec.maintrack});
+    if (spec.hasOwnProperty('track_id')){
+        that.track = Track({id:that.track_id});
     }
-    /***
-     * Initialise UI - bind main events etc.
-     *
-     * */
+    /**  Méthodes */
+
+    //initialise ui
     that.initialise = function() {
-        // event on click hideshow class
-        // hides or show the next div
+        // event on click hideshow class // hides or show the next div
         $(".hideshow").click(
             function(e){
                 if ($(this).next().css("display")=="none") {
@@ -33,24 +34,45 @@ var Ui = function(spec,my) {
                 else {$(this).next().hide(0); $(this).next().width(0)};
             }
         );
-        this.resize();
-        this.mainmap = Map({id:1, map_div: spec.mainmap_div, type: 'ol'});
-        this.mainmap.zoomToExtent(bounds,true);
+        $(".resize").on("dblclick", function() {that.resizeUi()});
+        $(window).resize(function() {
+            that.resizeUi();
+        });
+        this.resizeUi();
+
+        // Map init stuff
+        if (this.type === 'track') {
+            this.mainmap = SerpentineMap({id:1, map_div: spec.mainmap_div, type: 'ol'},{});
+            this.mainmap.initialise();
+        }
+        // Track stuff
+        if (this.track !== undefined && this.type === 'track') {
+            this.mainmap.drawTrack(this.mainmap, this.track);
+        }
+
     }
 
-    that.resize = function()  {
-        $("#main_content").width($("#content").width()-$("#sidebar").width());
-        $("#map_canvas").width($("#main_content").width());
-        $("#map_canvas").height($(window).height()-$("#header").height() -$("#charts").height() -$(".user_message").outerHeight(true));
-        $("#charts").width($("#main_content").width());
-        $("#chartdiv").width($("#main_content").width());
+    // auto resize
+    that.resizeUi = function()  {
+        if ($("#header").outerWidth()*0.3 > 300) {
+        $("#sidebar").outerWidth($("#header").outerWidth()*0.3);
+        } else {$("#sidebar").outerWidth(300);}
+        $("#main_content").outerWidth($("#content").outerWidth(true)-$("#sidebar").outerWidth(true)-1);
+
+        $("#map_canvas").outerWidth($("#main_content").outerWidth())
+        $("#charts").outerWidth($("#map_canvas").outerWidth());
+        $("#map_canvas").outerHeight($(window).height()-$("#header").outerHeight(true) -$("#charts").outerHeight(true) -$(".user_message").outerHeight(true));
+
         if (typeof this.mainmap != 'undefined') { this.mainmap.updateSize(); }
+        console.log('resizeUI');
     }
 
     that.toString = function() {
         return "UI - "+this.mainmap_div + ' height:'+ this.height;
 
     }
+
+    //return instance of that
     return that;
 }
 
@@ -83,20 +105,54 @@ var Track = function(spec,my) {
     return that;
 }
 
-var Map = function(spec,my) {
-    cartographic = new OpenLayers.Projection("EPSG:900913");
-    geographic = new OpenLayers.Projection("EPSG:4326");
-    bounds =  new OpenLayers.Bounds(-2,42,4,51).transform(geographic, cartographic);
-
-    var options = {
-	    projection: cartographic,
-	    displayProjection: geographic,
+var SerpentineMap = function(spec,my) {
+    var that = {};
+    if (spec.type === "ign") {
+        //chargement ign avec viewer getMap()
+    } else {
+        that = new OpenLayers.Map(spec.map_div);
+    }
+    that.cartographic = new OpenLayers.Projection("EPSG:900913");
+    that.geographic = new OpenLayers.Projection("EPSG:4326");
+    that.options = {
+	    projection: that.cartographic,
+	    displayProjection: that.geographic,
 	    units: "m"
     };
-    var map = new OpenLayers.Map(spec.map_div,options);
-    //map.cartographic = cartographic;
-    //map.geographic = geographic;
-    var mapnik = new OpenLayers.Layer.OSM("osm mapnik");
-    map.addLayer(mapnik);
-    return map;
+
+    that.initialise = function() {
+        this.addLayer(new OpenLayers.Layer.OSM("osm mapnik"));
+        var bounds =  new OpenLayers.Bounds(-2,42,4,51).transform(this.geographic, this.cartographic);
+        this.zoomToExtent(bounds,true);
+    }
+
+    that.drawTrack = function(map, track, options) {
+        options = (options ===  undefined) ? {} : options;
+        if (track.vector != undefined) {
+              map.removeLayer(track.vector);
+        }
+        track.vector = new OpenLayers.Layer.Vector(track.name);
+        var idxmin =  (options.hasOwnProperty('idxmin')) ? options.idxmin : 0;
+        var idxmax =  (options.hasOwnProperty('idxmax')) ? options.idxmax : 9999999999999;
+        var zoom =  (options.hasOwnProperty('zoom')) ? options.zoom : true; // par défaut on zoome sur la ligne
+
+        var track_points = [];
+        for (i in track.points) {
+            if (parseInt(i) >= parseInt(idxmin) && parseInt(i) < parseInt(idxmax)) {
+                var point = new OpenLayers.Geometry.Point(track.points[i]["lon"],  track.points[i]["lat"]);
+                track_points.push(point.transform(map.geographic,map.getProjectionObject()));
+            }
+        }
+        var line_style = (options.hasOwnProperty('style')) ? option.style : { strokeWidth: 3, strokeColor: 'blue', strokeOpacity: 0.7 };
+        var lineGeometry =   new OpenLayers.Geometry.LineString(track_points);
+        var lineFeature = new OpenLayers.Feature.Vector(lineGeometry,null,line_style);
+        track.vector.addFeatures(lineFeature);
+        map.addLayer(track.vector);
+        if (zoom) {
+            map.zoomToExtent(lineGeometry.getBounds());
+        }
+        return lineGeometry;
+    }
+
+    return that;
 }
