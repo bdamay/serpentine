4f7c1706c73235b891ffe5fb7599d190e77e1839
bdamay <bdamay@gmail.com>
2015-12-18 10:05:34 +0100

    settings

--- serpentine/settings.py
+++ serpentine/settings.py
@@ -12,7 +12,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'C:/benoit/django/serpentine/spt.db3',                      # Or path to database file if using sqlite3.
+        'NAME': 'C:/Serpentine/trunk/spt.db3',                      # Or path to database file if using sqlite3.
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
